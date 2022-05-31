92cd6ab938590eac33317d9441487a4d9261a5ba
abdo <abdelrahman.nashat199@gmail.com>
2022-05-31 14:53:44 +0200

    add ExpenseComponent

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import Card from "../UI/Card.jsx";
 import ExpenseDate from "./ExpenseDate.jsx";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   console.log(props);
+  const [title, setTitle] = useState(props.title);
   const changeTitle = () => {
-    console.log("test");
+    setTitle("updated");
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}$</div>
       </div>
       <button onClick={changeTitle}>Change title</button>
